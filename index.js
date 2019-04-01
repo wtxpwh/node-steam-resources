8b71ed69ea79f89f84d3b768c6442776066f3b34
wtxpwh <wtxpwh@gmail.com>
2019-04-01 16:40:21 +0800

    update protobuf.js version 5

--- index.js
+++ index.js
@@ -37,17 +37,9 @@ for (var namespace in namespaces) {
 require('./steam_language_parser');
 
 function loadProtoFiles(paths) {
-  //protobufjs 6
-  var root;
-  paths.forEach(function (path) {
-    root = ProtoBuf.loadSync(__dirname + '/protobufs/' + path, root);
-  })
-  return root;
-  /*protobufjs 4 and 5
   var builder = ProtoBuf.newBuilder();
   paths.forEach(function(path) {
     ProtoBuf.loadProtoFile(__dirname + '/protobufs/' + path, builder);
   });
   return builder.build();
-  */
 }
